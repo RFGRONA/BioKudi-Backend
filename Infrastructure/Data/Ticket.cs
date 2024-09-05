2dc7b37e4ea36b937f241ed35073676e8f3f0448
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-09-04 22:47:43 -0500

    DB, update SQL scripts

---
+++ Infrastructure/Data/Ticket.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Biokudi_Backend.Infrastructure;
+
+public partial class Ticket
+{
+    public int IdTicket { get; set; }
+
+    public string Affair { get; set; } = null!;
+
+    public DateTime DateCreated { get; set; }
+
+    public DateTime? DateAnswered { get; set; }
+
+    public string? AnsweredBy { get; set; }
+
+    public bool? ScalpAdmin { get; set; }
+
+    public int PersonId { get; set; }
+
+    public int? StateId { get; set; }
+
+    public int? TypeId { get; set; }
+
+    public virtual Person Person { get; set; } = null!;
+
+    public virtual ICollection<Picture> Pictures { get; set; } = new List<Picture>();
+
+    public virtual CatState? State { get; set; }
+
+    public virtual CatType? Type { get; set; }
+}
