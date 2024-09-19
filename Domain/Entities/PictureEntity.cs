6eb176ffbb928a48253b0e22247511485359836b
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-09-18 19:54:30 -0500

    RF, entities updated

--- Domain/Entities/PictureEntity.cs
+++ Domain/Entities/PictureEntity.cs
@@ -5,18 +5,31 @@ namespace Biokudi_Backend.Domain.Entities
     public class PictureEntity
     {
         public int IdPicture { get; set; }
-        public string Name { get; set; }
-        public string Link { get; set; }
-        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+
+        public string Name { get; set; } = null!;
+
+        public string Link { get; set; } = null!;
+
+        public DateTime? DateCreated { get; set; }
+
         public int? TypeId { get; set; }
-        public CatType Type { get; set; }
+
         public int? PlaceId { get; set; }
-        public Place Place { get; set; }
+
         public int? PersonId { get; set; }
-        public Person Person { get; set; }
+
         public int? TicketId { get; set; }
-        public Ticket Ticket { get; set; }
+
         public int? ReviewId { get; set; }
-        public Review Review { get; set; }
+
+        public virtual PersonEntity? Person { get; set; }
+
+        public virtual PlaceEntity? Place { get; set; }
+
+        public virtual ReviewEntity? Review { get; set; }
+
+        public virtual TicketEntity? Ticket { get; set; }
+
+        public virtual CatTypeEntity? Type { get; set; }
     }
 }
--- Domain/Entities/PlaceEntity.cs
+++ Domain/Entities/PlaceEntity.cs
@@ -6,19 +6,23 @@ namespace Biokudi_Backend.Domain.Entities
     public class PlaceEntity
     {
         public int IdPlace { get; set; }
-        public string NamePlace { get; set; }
+        public string NamePlace { get; set; } = null!;
         public decimal Latitude { get; set; }
         public decimal Longitude { get; set; }
-        public string Address { get; set; }
-        public string Description { get; set; }
-        public string Link { get; set; }
-        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
-        public DateTime? DateModified { get; set; } = DateTime.UtcNow;
-        public int CityId { get; set; }
-        public CatCity City { get; set; }
-        public int StateId { get; set; }
-        public CatState State { get; set; }
-        public ICollection<CatActivityEntity> Activities { get; set; }  
-        public ICollection<ReviewEntity> Reviews { get; set; }
+        public string Address { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Link { get; set; } = null!;
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateModified { get; set; } 
+        public int? CityId { get; set; }
+        public int? StateId { get; set; }
+        public CatCityEntity? City { get; set; }
+        public ICollection<PictureEntity> Pictures { get; set; } = [];
+        public ICollection<PostEntity> Posts { get; set; } = [];
+        public ICollection<ReviewEntity> Reviews { get; set; } = [];
+        public CatStateEntity? State { get; set; }
+        public ICollection<CatActivityEntity> Activities { get; set; } = [];
+        public ICollection<ListEntity> Lists { get; set; } = [];
+        public double Rating { get; set; }
     }
 }
