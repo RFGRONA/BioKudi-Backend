8c24a116a20ce1f4b247fbc3334bade6874d366e
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-09-18 19:55:18 -0500

    CF, mappings added

---
+++ Application/Mappings/PlaceMapping.cs
@@ -0,0 +1,19 @@
+﻿using Biokudi_Backend.Application.DTOs.Response;
+using Biokudi_Backend.Domain.Entities;
+
+namespace Biokudi_Backend.Application.Mappings
+{
+    public class PlaceMapping
+    {
+        public StartCarrouselDto MapPlaceToCarrouselDto(PlaceEntity place)
+        {
+            return new StartCarrouselDto
+            {
+                name = place.NamePlace,
+                rating = place.Rating,
+                url = place.Pictures.FirstOrDefault()?.Link
+                      ?? "https://i.postimg.cc/8kHXP1bz/dan-gold-Uuh0-K9-Yt-NM8-unsplash-1.jpg"
+            };
+        }
+    }
+}
