a8f13c615ccac2edd7a89988721ad50ad97e8b87
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-09-18 19:54:54 -0500

    CF, cache service interface added

---
+++ Application/Interfaces/ICacheService.cs
@@ -0,0 +1,11 @@
+﻿namespace Biokudi_Backend.Application.Interfaces
+{
+    public interface ICacheService
+    {
+        T? Get<T>(string key);
+        IEnumerable<T>? GetCollection<T>(string key);
+        void Set<T>(string key, T value, TimeSpan expirationTime);
+        void SetCollection<T>(string key, IEnumerable<T> value, TimeSpan expirationTime);
+        void Remove(string key);
+    }
+}
