f063c2a31e6955e5457d6b7471b50d4e23adbdfe
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-09-18 19:55:08 -0500

    RF, interfaces updated

--- Application/Interfaces/IPersonService.cs
+++ Application/Interfaces/IPersonService.cs
@@ -1,6 +1,11 @@
-﻿namespace Biokudi_Backend.Application.Interfaces
+﻿using Biokudi_Backend.Application.DTOs.Request;
+using Biokudi_Backend.Application.DTOs.Response;
+
+namespace Biokudi_Backend.Application.Interfaces
 {
     public interface IPersonService
     {
+        Task<LoginResponseDto> LoginPerson(LoginRequestDto loginDto);
+        Task<RegisterRequestDto> RegisterPerson(RegisterRequestDto registerDto);
     }
 }
