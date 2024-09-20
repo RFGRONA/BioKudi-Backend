0cd64e0d346662b720dff38abebc84f94c52f217
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-09-19 19:15:36 -0500

    RF, person entity updated

--- Domain/Entities/PersonEntity.cs
+++ Domain/Entities/PersonEntity.cs
@@ -8,19 +8,27 @@ namespace Biokudi_Backend.Domain.Entities
         public string NameUser { get; set; }
         public string Telephone { get; set; }
         public string Email { get; set; }
-        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
-        public DateTime? DateModified { get; set; } = DateTime.UtcNow;
+        public DateTime DateCreated { get; set; } 
+        public DateTime? DateModified { get; set; } 
         public string Password { get; set; }
-        public string Hash { get; set; }
-        public int StateId { get; set; }
-        public CatState State { get; set; }
-        public int RoleId { get; set; }
-        public CatRole Role { get; set; }
-        public bool AccountDeleted { get; set; } = false;
-        public bool EmailNotification { get; set; } = true;
-        public bool EmailPost { get; set; } = true;
-        public bool EmailList { get; set; } = true;
-        public ICollection<TicketEntity> Tickets { get; set; }
-        public ICollection<ReviewEntity> Reviews { get; set; }
+        public string Hash { get; set; } = "0";
+        public int? StateId { get; set; } = 1;
+        public int RoleId { get; set; } = 1;
+        public bool? AccountDeleted { get; set; } = false;
+        public bool? EmailNotification { get; set; } = true;
+        public bool? EmailPost { get; set; } = true;
+        public bool? EmailList { get; set; } = true;
+        public ICollection<List> Lists { get; set; } = [];
+        public ICollection<Picture> Pictures { get; set; } = [];
+        public ICollection<Review> Reviews { get; set; } = [];
+        public CatRole Role { get; set; } = null!;
+        public CatState? State { get; set; }
+        public ICollection<Ticket> Tickets { get; set; } = [];
+
+        public static DateTime DateNowColombia()
+        {
+            TimeZoneInfo timeZoneColombia = TimeZoneInfo.FindSystemTimeZoneById("SA Pacific Standard Time");
+            return TimeZoneInfo.ConvertTime(DateTime.Now, timeZoneColombia);
+        }
     }
 }
