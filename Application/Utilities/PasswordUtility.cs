92b8a65e2d854ee679c94e62d3e8f9b3a1847351
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-09-19 19:19:03 -0500

    RF, utilities updated

---
+++ Application/Utilities/PasswordUtility.cs
@@ -0,0 +1,16 @@
+﻿namespace Biokudi_Backend.Application.Utilities
+{
+    using BCrypt.Net;
+    public class PasswordUtility
+    {
+        public static string HashPassword(string password)
+        {
+            return BCrypt.HashPassword(password);
+        }
+
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            return BCrypt.Verify(password, hashedPassword);
+        }
+    }
+}
