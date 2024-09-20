c567b5275a69ca8742f6250e1e2a365ef9109c57
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-09-19 19:19:35 -0500

    CF, config files added

--- Infrastructure/Config/AuthConfig.cs
+++ Infrastructure/Config/AuthConfig.cs
@@ -1,6 +1,45 @@
-﻿namespace Biokudi_Backend.Infrastructure.Config
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Biokudi_Backend.Infrastructure.Config
 {
-    public class AuthConfig
+    public static class AuthConfig
     {
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtKey = configuration["Jwt:Key"];
+            if (string.IsNullOrEmpty(jwtKey)) throw new ArgumentNullException(nameof(jwtKey), "JWT key cannot be null or empty.");
+            var key = Encoding.ASCII.GetBytes(jwtKey);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                };
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var token = context.Request.Cookies["jwt"];
+                        if (!string.IsNullOrEmpty(token))
+                            context.Token = token;
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+
+            return services;
+        }
     }
 }
---
+++ Infrastructure/Config/CORSConfig.cs
@@ -0,0 +1,29 @@
+﻿namespace Biokudi_Backend.Infrastructure.Config
+{
+    public static class CORSConfig
+    {
+        public static IServiceCollection AddCustomCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllAllowed", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                });
+
+                options.AddPolicy("OnlyFrontend", policy =>
+                {
+                    policy.WithOrigins("https://biokudi.vercel.app")
+                          .AllowAnyMethod()
+                          .AllowAnyHeader()
+                          .AllowCredentials();
+                });
+
+            });
+
+            return services;
+        }
+    }
+}
