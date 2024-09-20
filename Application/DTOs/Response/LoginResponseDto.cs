ee5e8a594878916b5f569ec5640047e34e0e9b95
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-09-19 19:15:50 -0500

    RF, dto updated

--- Application/DTOs/Response/LoginResponseDto.cs
+++ Application/DTOs/Response/LoginResponseDto.cs
@@ -2,8 +2,8 @@
 {
     public class LoginResponseDto
     {
-        public string Token { get; set; }
         public string NameUser { get; set; }
+        public int UserId { get; set; }
         public string Email { get; set; }
         public string Role { get; set; }
     }
