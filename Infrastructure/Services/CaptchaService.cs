aca479f2977c02a5c292c775d0bb05ec7dc7a2e6
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-09-23 21:00:54 -0500

    FX, validation reCaptcha v2

--- Infrastructure/Services/CaptchaService.cs
+++ Infrastructure/Services/CaptchaService.cs
@@ -3,21 +3,33 @@ using Newtonsoft.Json;
 
 namespace Biokudi_Backend.Infrastructure.Services
 {
-    public class CaptchaService(IConfiguration configuration, HttpClient httpClient)
+    public class CaptchaService
     {
-        private readonly string _secretKey = configuration["ApiKeys:CaptchaKey"] ?? throw new ArgumentNullException(nameof(configuration), "CaptchaKey cannot be null");
-        private readonly HttpClient _httpClient = httpClient;
+        private readonly string _secretKey;
+        private readonly HttpClient _httpClient;
+
+        public CaptchaService(IConfiguration configuration, HttpClient httpClient)
+        {
+            _secretKey = configuration["ApiKeys:CaptchaKey"] ?? throw new ArgumentNullException(nameof(configuration), "CaptchaKey cannot be null");
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
 
         public async Task<bool> VerifyCaptcha(string captchaToken)
         {
-            var response = await _httpClient.PostAsync($"https://www.google.com/recaptcha/api/siteverify?secret={_secretKey}&response={captchaToken}", null);
+            if (string.IsNullOrEmpty(captchaToken))
+                throw new ArgumentException("Captcha token cannot be null or empty", nameof(captchaToken));
+            var postData = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("secret", _secretKey),
+                new KeyValuePair<string, string>("response", captchaToken)
+            });
+            var response = await _httpClient.PostAsync("https://www.google.com/recaptcha/api/siteverify", postData);
             if (!response.IsSuccessStatusCode)
                 return false;
-
             var jsonResponse = await response.Content.ReadAsStringAsync();
             var captchaResult = JsonConvert.DeserializeObject<CaptchaResponseDto>(jsonResponse);
-
-            return captchaResult?.Success == true && captchaResult.Score > 0.5;
+            return captchaResult?.Success == true;
         }
+
     }
 }
