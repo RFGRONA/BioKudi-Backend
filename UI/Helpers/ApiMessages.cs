0554ae8821725492c80f15a579d1fed695641297
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-09-23 21:00:40 -0500

    MN, file renamed

--- UI/Helpers/ApiMessages.cs
+++ UI/Helpers/ApiMessages.cs
@@ -1,11 +1,12 @@
 ﻿namespace Biokudi_Backend.UI.Helpers
 {
-    public static class PersonMessages
+    public static class ApiMessages
     {
         public const string CaptchaInvalid = "Captcha Invalido";
         public const string InvalidModel = "Modelo Inválido";
         public const string InvalidSession = "Sesión Inválida";
         public const string Logout = "Sesión Cerrada";
         public const string PersonNotFound = "Usuario no encontrado";
+        public const string PublicKeyError = "Error al obtener la llave pública";
     }
 }
