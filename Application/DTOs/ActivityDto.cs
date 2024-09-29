7f38b055a21ba242978d6e92c0fac15bed7f99e9
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-09-29 11:58:56 -0500

    CF, DTOs added

---
+++ Application/DTOs/ActivityDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Biokudi_Backend.Application.DTOs
+{
+    public class ActivityDto
+    {
+        public int IdActivity { get; set; }
+        public string NameActivity { get; set; } = null!;
+        public string UrlIcon { get; set; } = null!;
+    }
+
+    public class ActivityRequestDto
+    {
+        public string NameActivity { get; set; } = null!;
+        public string UrlIcon { get; set; } = null!;
+    }
+
+    public class IdActivityDto
+    {
+        public int IdActivity { get; set; }
+    }
+}
