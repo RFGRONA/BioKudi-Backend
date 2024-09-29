c39a4499f7c58915f3bb5e1660ad21aab227ae5c
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-09-29 12:02:16 -0500

    CF, methods added

--- Application/Services/PlaceService.cs
+++ Application/Services/PlaceService.cs
@@ -2,6 +2,9 @@
 using Biokudi_Backend.Application.Interfaces;
 using Biokudi_Backend.Domain.Interfaces;
 using Biokudi_Backend.Application.Mappings;
+using Biokudi_Backend.Application.DTOs;
+using Biokudi_Backend.Infrastructure.Repositories;
+using Biokudi_Backend.Application.DTOs.Request;
 
 namespace Biokudi_Backend.Application.Services
 {
@@ -9,6 +12,68 @@ namespace Biokudi_Backend.Application.Services
     {
         private readonly IPlaceRepository _placeRepository = _placeRepository;
         private readonly PlaceMapping _placeMapping = _placeMapping;
+
+        public async Task<bool?> CreatePlace(PlaceRequestDto place)
+        {
+            try
+            {
+                var result = await _placeRepository.Create(_placeMapping.RequestToPlaceEntity(place));
+                if (result != null)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<bool> DeletePlace(int id)
+        {
+            try
+            {
+                var result = await _placeRepository.Delete(id);
+                if (result)
+                    return true;
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<List<PlaceListCrudDto>?> GetCrudPlaces()
+        {
+            try
+            {
+                var result = await _placeRepository.GetAll();
+                var places = result
+                .OrderBy(p => p.IdPlace)
+                .Select(p => _placeMapping.PlaceToCrudList(p))
+                .ToList() ?? null;
+                return places;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<PlaceDetailResponseDto?> GetPlaceById(int id)
+        {
+            try
+            {
+                var result = await _placeRepository.GetById(id) ?? null;
+                return _placeMapping.MapToPlaceDetailResponseDto(result);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
 
         public async Task<List<StartCarrouselDto>?> GetStartCarrousel()
         {
@@ -27,5 +92,23 @@ namespace Biokudi_Backend.Application.Services
             return startCarrousel;
         }
 
+        public async Task<bool?> UpdatePlace(int id, PlaceRequestDto place)
+        {
+            try
+            {
+                var entity = _placeMapping.RequestToPlaceEntity(place);
+                entity.IdPlace = id;
+                var result = await _placeRepository.Update(entity);
+                if (result != null)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
