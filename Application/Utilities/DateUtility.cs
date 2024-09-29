d6ef29c48617ff8bc2d511aab8ce18d803e13824
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-09-29 12:02:28 -0500

    CF, date utility added

---
+++ Application/Utilities/DateUtility.cs
@@ -0,0 +1,11 @@
+﻿namespace Biokudi_Backend.Application.Utilities
+{
+    public static class DateUtility
+    {
+        public static DateTime DateNowColombia()
+        {
+            TimeZoneInfo timeZoneColombia = TimeZoneInfo.FindSystemTimeZoneById("SA Pacific Standard Time");
+            return TimeZoneInfo.ConvertTime(DateTime.Now, timeZoneColombia);
+        }
+    }
+}
