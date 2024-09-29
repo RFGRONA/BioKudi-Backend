de315bd65876e099db1f9c38e1d771f0447a21de
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-09-29 12:00:20 -0500

    CF, mappings added

---
+++ Application/Mappings/DepartmentMapping.cs
@@ -0,0 +1,35 @@
+﻿using Biokudi_Backend.Application.DTOs;
+using Biokudi_Backend.Domain.Entities;
+
+namespace Biokudi_Backend.Application.Mappings
+{
+    public class DepartmentMapping
+    {
+        public CatDepartmentEntity DtoToEntity(DepartmentDto department)
+        {
+            return new CatDepartmentEntity
+            {
+                IdDepartment = department.IdDepartment,
+                NameDepartment = department.NameDepartment
+            };
+        }
+
+        public CatDepartmentEntity RequestToEntity(DepartmentRequestDto department)
+        {
+            return new CatDepartmentEntity
+            {
+                NameDepartment = department.NameDepartment
+            };
+        }
+
+        public DepartmentDto EntityToDto(CatDepartmentEntity department)
+        {
+            return new DepartmentDto
+            {
+                IdDepartment = department.IdDepartment,
+                NameDepartment = department.NameDepartment
+            };
+        }
+
+    }
+}
--- Application/Mappings/PlaceMapping.cs
+++ Application/Mappings/PlaceMapping.cs
@@ -1,4 +1,5 @@
-﻿using Biokudi_Backend.Application.DTOs.Response;
+﻿using Biokudi_Backend.Application.DTOs.Request;
+using Biokudi_Backend.Application.DTOs.Response;
 using Biokudi_Backend.Domain.Entities;
 
 namespace Biokudi_Backend.Application.Mappings
@@ -13,6 +14,55 @@ namespace Biokudi_Backend.Application.Mappings
                 rating = place.Rating,
                 url = place.Pictures.FirstOrDefault()?.Link
                       ?? "https://i.postimg.cc/8kHXP1bz/dan-gold-Uuh0-K9-Yt-NM8-unsplash-1.jpg"
+            };
+        }
+
+        public PlaceListCrudDto PlaceToCrudList(PlaceEntity place)
+        {
+            return new PlaceListCrudDto
+            {
+                IdPlace = place.IdPlace,
+                NamePlace = place.NamePlace,
+                Link = place.Link,
+                CityName = place.City?.NameCity,
+                StateName = place.State?.NameState
+            };
+        }
+
+        public PlaceDetailResponseDto MapToPlaceDetailResponseDto(PlaceEntity place)
+        {
+            return new PlaceDetailResponseDto
+            {
+                IdPlace = place.IdPlace,
+                NamePlace = place.NamePlace,
+                Link = place.Link,
+                Address = place.Address,
+                DateCreated = place.DateCreated,
+                DateModified = place.DateModified,
+                Latitude = place.Latitude,
+                Longitude = place.Longitude,
+                Description = place.Description,
+                CityName = place.City?.NameCity,
+                StateName = place.State?.NameState
+            };
+        }
+
+        public PlaceEntity RequestToPlaceEntity(PlaceRequestDto place)
+        {
+            return new PlaceEntity
+            {
+                NamePlace = place.NamePlace,
+                Latitude = place.Latitude,
+                Longitude = place.Longitude,
+                Address = place.Address,
+                Description = place.Description,
+                Link = place.Link,
+                StateId = place.StateId,
+                CityId = place.CityId,
+                Activities = place.Activities.Select(a => new CatActivityEntity
+                {
+                    IdActivity = a.IdActivity
+                }).ToList()
             };
         }
     }
