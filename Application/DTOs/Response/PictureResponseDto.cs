a24b3c2f73c6d6cefdddf0d597b363864d4ebb83
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-10-20 23:16:23 -0500

    CF, Picture CRUD added

---
+++ Application/DTOs/Response/PictureResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Biokudi_Backend.Application.DTOs.Response
+{
+    public class PictureResponseDto
+    {
+        public int IdPicture { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string TypeName { get; set; }
+    }
+}
---
+++ UI/Controllers/PictureController.cs
@@ -0,0 +1,36 @@
+﻿using Biokudi_Backend.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biokudi_Backend.UI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize(Roles = "Admin")]
+    public class PictureController(IPictureService pictureService) : ControllerBase
+    {
+        private readonly IPictureService _pictureService = pictureService;
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _pictureService.GetPictures();
+            if (result.IsFailure)
+                return BadRequest(result.ErrorMessage);
+            if (result.Value == null || !result.Value.Any())
+                return NotFound();
+
+            return Ok(result.Value);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _pictureService.DeletePicture(id);
+            if (result.IsFailure)
+                return BadRequest(result.ErrorMessage);
+
+            return Ok();
+        }
+    }
+}
