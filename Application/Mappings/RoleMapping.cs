22a4607854eb01e6971919a67c9f2814e3ba4c0d
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-10-20 23:15:24 -0500

    CF, Role CRUD added

---
+++ Application/Mappings/RoleMapping.cs
@@ -0,0 +1,34 @@
+﻿using Biokudi_Backend.Application.DTOs;
+using Biokudi_Backend.Domain.Entities;
+
+namespace Biokudi_Backend.Application.Mappings
+{
+    public class RoleMapping
+    {
+        public CatRoleEntity DtoToEntity(RoleDto role)
+        {
+            return new CatRoleEntity
+            {
+                IdRole = role.IdRole,
+                NameRole = role.NameRole
+            };
+        }
+
+        public CatRoleEntity RequestToEntity(RoleRequestDto role)
+        {
+            return new CatRoleEntity
+            {
+                NameRole = role.NameRole
+            };
+        }
+
+        public RoleDto EntityToDto(CatRoleEntity role)
+        {
+            return new RoleDto
+            {
+                IdRole = role.IdRole,
+                NameRole = role.NameRole
+            };
+        }
+    }
+}
