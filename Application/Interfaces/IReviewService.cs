da4d74185ee4c59ea218a48809c30d0c6a009bb8
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-10-20 23:16:52 -0500

    CF, ReviewService added

--- Application/Interfaces/IReviewService.cs
+++ Application/Interfaces/IReviewService.cs
@@ -1,6 +1,16 @@
-﻿namespace Biokudi_Backend.Application.Interfaces
+﻿using Biokudi_Backend.Application.DTOs.Request;
+using Biokudi_Backend.Application.DTOs.Response;
+using Biokudi_Backend.Domain.ValueObject;
+
+namespace Biokudi_Backend.Application.Interfaces
 {
     public interface IReviewService
     {
+        Task<Result<ReviewResponseDto>> CreateReviewAsync(CreateReviewRequestDto dto);
+        Task<Result<bool>> UpdateReviewAsync(int id, UpdateReviewRequestDto dto);
+        Task<Result<bool>> DeleteReviewAsync(int id);
+        Task<Result<ReviewResponseDto>> GetReviewByIdAsync(int id);
+        Task<Result<IEnumerable<ReviewResponseDto>>> GetAllReviewsAsync();
+        Task<Result<IEnumerable<ReviewResponseDto>>> GetReviewsByPlaceIdAsync(int placeId);
     }
 }
