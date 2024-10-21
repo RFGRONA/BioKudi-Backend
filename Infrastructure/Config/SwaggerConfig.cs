4bfb7f7256bd68135e765046f37fe468a3748809
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-10-20 23:06:37 -0500

    CF, Swagger Config added

---
+++ Infrastructure/Config/SwaggerConfig.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Biokudi_Backend.Infrastructure.Config
+{
+    public static class SwaggerConfig
+    {
+        public static void ConfigureSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Biokudi API",
+                    Version = "v1",
+                    Description = "Gestión y validación de datos."
+                });
+
+                // Configuración de seguridad para JWT
+                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Token usar Bearer {token}",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "Bearer",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
+            });
+        }
+    }
+}
