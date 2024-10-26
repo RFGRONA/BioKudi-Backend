0fd3c09bfe9b4b77c875079b9670a215994a1129
RFGRONA <gabrielemartinez@ucundinamarca.edu.co>
2024-10-25 22:38:43 -0500

    CF, Status controller

---
+++ Application/DTOs/Response/StatusDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Biokudi_Backend.Application.DTOs.Response
+{
+    public class StatusDto
+    {
+        public string Status { get; set; } = "Healthy"; 
+        public string? Version { get; set; }
+        public TimeSpan Uptime { get; set; }
+        public bool DatabaseConnection { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
